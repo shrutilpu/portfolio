05f43a067d3d2ded249213b729750f3990191546
Shruti kesharwani <49159599+shrutilpu@users.noreply.github.com>
2023-05-16 17:01:18 +0530

    feat:added-experience

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,7 +4,25 @@ class Portfolio extends Component {
   render() {
 
     if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
+      var projects = this.props.data.projects.slice(0,4).map(function(projects){
+        var projectImage = 'images/portfolio/'+projects.image;
+        return <div key={projects.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a href={projects.url} title={projects.title}>
+               <img alt={projects.title} src={projectImage} />
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{projects.title}</h5>
+                     <p>{projects.category}</p>
+                  </div>
+                </div>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
+          </div>
+        </div>
+      })
+
+      var oldProjects = this.props.data.projects.slice(4).map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
@@ -31,10 +49,21 @@ class Portfolio extends Component {
          <div className="twelve columns collapsed">
 
             <h1>Check Out Some of My Works.</h1>
-
+            <h4>Latest Projects</h4>
+            <br/>
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+           
                 {projects}
+              
             </div>
+            <h4>Older Projects</h4>
+            <br/>
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+           
+            {oldProjects}
+              
+            </div>
+                
           </div>
       </div>
    </section>
